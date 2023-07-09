871ab856c2e22b823a3930a1a97a6e7182684b40
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-07-09 20:03:32 +0000

    added serializers

---
+++ LittleLemonApi/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Order,OrderItem,MenuItem,Cart,Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = "__all__"
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = "__all__"
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = "__all__"
+
