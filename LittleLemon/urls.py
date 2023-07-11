2fe6796f85f771e32cbb22d95aef341de16ce977
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-07-11 17:26:05 +0000

    setting up booking and menu urls

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import BookingViewSet
 
+router.register('tables', views.BookingViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path("api/",include("LittleLemonApi.urls")),
-    path('restaurant/', include("restaurant.urls")),
+    path('restaurant/menu', include("restaurant.urls")),
+    
+    path('restaurant/booking/', include(router.urls)),
 ]
